cfcc12394afe6d89271338124d2ad0af6b1dc835
Clément Godrie <clemgod5@gmail.com>
2024-08-23 21:42:40 +0200

    23/08/24

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('chat/<int:id>/', views.chat, name="chat"),
-    path('chat/<int:id>/send-message/', views.send_message, name="send-message"),
-    path('chat/<int:id>/get-messages/', views.get_messages, name="get-messages"),
+    path('histoire', views.history, name="history"),
+    path('objectifs', views.objectives, name="objectives"),
+    path('ag-ca', views.agca, name="ag-ca"),
+    path('contact', views.contact, name="contact"),
+    path('evenements', views.events, name="events"),
+    path('ca-zone-privée', views.caprivatezone, name="ca-private-zone"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -7,61 +7,20 @@ from django.http import JsonResponse
 def home(request):
     return render(request, template_name='main/home.html')
 
-@login_required
-def chat(request, id):
+def history(request):
+    return render(request, template_name='main/history.html')
+    
+def objectives(request):
+    return render(request, template_name='main/objectives.html')
 
-    # Vérifier si la discussion existe déjà entre ces deux utilisateurs
-    discussion = Discussion.objects.filter(id=id)
+def agca(request):
+    return render(request, template_name='main/home.html')
 
-    if discussion.exists():
-        user_a = discussion.first().users.all()[0]
-        messages = Message.objects.filter(discussion=discussion.first())
-        if len(discussion.first().users.all()) > 2 and user_a != request.user:
-            discussion.users.add(request.user)
-    else:
-        # Si la discussion n'existe pas, vous pouvez choisir de la créer ici
-        # Notez que cela dépend de votre logique métier, vous pourriez créer la discussion à un autre endroit.
-        discussion = Discussion.objects.create()
-        discussion.users.add(request.user)
-        messages = Message.objects.none()  # Pas de messages pour une nouvelle discussion
+def contact(request):
+    return render(request, template_name='main/home.html')
 
-    return render(request, 'main/chat.html', {'discussion_id': discussion.first().id, 'users_messages': messages})
+def events(request):
+    return render(request, template_name='main/home.html')
 
-
-@csrf_exempt
-def send_message(request, id):
-    if request.method == 'POST':
-        message_content = request.POST.get('message_content', '')
-        try:
-            discussion = Discussion.objects.get(id=id)
-            new_message = Message.objects.create(discussion=discussion, user=request.user, content=message_content)
-
-            serialized_message = {
-                'user': new_message.user.username,
-                'message': new_message.content,
-                'sent_at': new_message.sent_at.strftime('%Y-%m-%dT%H:%M:%S'),
-            }
-
-            return JsonResponse({'status': 'success', 'message': serialized_message})
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
-    return JsonResponse({'error': 'Invalid request method'})
-
-
-@login_required
-def get_messages(request, id):
-    try:
-        messages = Message.objects.filter(discussion=Discussion.objects.get(id=id))
-
-        # Serialize messages
-        serialized_messages = []
-        for message in messages:
-            serialized_message = {
-                'user': message.user.username,
-                'message': message.content,
-                'sent_at': message.sent_at.strftime('%Y-%m-%dT%H:%M:%S'),
-            }
-            serialized_messages.append(serialized_message)
-        return JsonResponse({'status': 'success', 'messages': serialized_messages})
-    except Exception as e:
-        return JsonResponse({'status': 'error', 'error_message': str(e)})
+def caprivatezone(request):
+    return render(request, template_name='main/home.html')
